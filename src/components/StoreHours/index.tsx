9159415633d4946430c71ccc65dd54a89738d5a8
Drew Powers <drew@pow.rs>
2019-11-10 21:40:21 -0700

    Update store hours

--- src/components/StoreHours/index.tsx
+++ src/components/StoreHours/index.tsx
@@ -5,22 +5,33 @@ import * as Styled from './styles';
 interface Location {
   name: string;
   address: string;
-  opening: number;
-  militaryHours: ([number, number])[];
+  opening: string;
+  militaryHours: [
+    [number, number],
+    [number, number],
+    [number, number],
+    [number, number],
+    [number, number],
+    [number, number],
+    [number, number]
+  ];
+  tz: number;
 }
 
 const locations: Location[] = [
   {
     name: 'East End Market',
     address: '3201 Corrine Dr',
-    opening: 20130901,
+    opening: '2013-09-01',
     militaryHours: [[8, 18], [8, 19], [8, 19], [8, 19], [8, 19], [8, 19], [8, 19]],
+    tz: 5, // EST
   },
   {
     name: 'Mills/50',
     address: '1011 E Colonial Dr',
-    opening: 20171202,
-    militaryHours: [[7, 19], [7, 19], [7, 19], [7, 19], [7, 19], [7, 19], [7, 19]],
+    opening: '2017-12-02',
+    militaryHours: [[7, 21], [7, 21], [7, 21], [7, 21], [7, 21], [7, 21], [7, 21]],
+    tz: 5, // EST
   },
 ];
 
@@ -28,95 +39,97 @@ const format = (hours: number) => {
   if (hours === 12) {
     return `${hours}p`;
   }
-  if (hours >= 13) {
-    return `${hours - 12}p`;
-  }
-  return `${hours}a`;
+  return hours >= 13 ? `${hours - 12}p` : `${hours}a`;
 };
 
-const today = new Date();
-const offsetMin = today.getTimezoneOffset();
-const offsetHours = offsetMin / 60;
-let day = Math.floor(today.getUTCDay() + (today.getUTCHours() - offsetHours) / 24);
-if (day < 0) {
-  day = 6;
-} else if (day >= 7) {
-  day = 0;
-}
-
-const isOpen = (location: Location) => {
-  let hours = today.getUTCHours() - offsetHours; // can be > 24; makes late-night closing calculations and timezones simpler
-  hours = hours < 0 ? hours + 24 : hours;
-  let minutes = today.getUTCMinutes();
-  minutes = minutes < 0 ? minutes + 60 : minutes;
-
-  // Check if location opened
-  const month = today.getUTCMonth() < 9 ? `0${today.getUTCMonth() + 1}` : today.getUTCMonth() + 1;
-  const date = today.getUTCDate() < 10 ? `0${today.getUTCDate()}` : today.getUTCDate();
-  if (parseInt(`${today.getUTCFullYear()}${month}${date}`, 10) < location.opening) {
-    return 'Opening Soon';
+const StoreHours: React.FunctionComponent = () => {
+  function getDay(tz: number) {
+    let day = new Date().getUTCDay();
+    // if it’s just past midnight UTC, stay previous day UTC
+    if (new Date().getUTCHours() < tz) {
+      day -= 1;
+    }
+    // if we subtracted from Sunday, it’s Saturday
+    if (day < 0) {
+      day = 6;
+    }
+    return day;
   }
-  // Return closing soon if within 30 min
-  let open = location.militaryHours[day][0];
-  open = open < 0 ? open + 24 : open;
-  let close = location.militaryHours[day][1];
-  close = close < 0 ? close + 24 : close;
 
-  if (hours + minutes / 60 >= close - 0.5 && hours < close) {
-    return 'Closing Soon';
+  function isOpen({ opening, militaryHours, tz }: Location) {
+    // location hasn’t opened
+    const currentDate = new Date();
+    if (currentDate < new Date(opening)) {
+      return 'Opening Soon';
+    }
+
+    let currentHour = new Date().getUTCHours() - tz;
+    if (currentHour < 0) {
+      currentHour += 24;
+    }
+    const [open, close] = militaryHours[getDay(tz)];
+
+    // return closing soon if within last hour
+    if (close - currentHour === 1) {
+      return 'Closing Soon';
+    }
+    if (currentHour >= open && currentHour < close) {
+      return 'Open';
+    }
+    return 'Closed';
   }
-  if (hours >= open && hours < close) {
-    return 'Open';
-  }
-  return 'Closed';
+
+  return (
+    <Styled.Container>
+      <Styled.Heading>Locations</Styled.Heading>
+      <Styled.Grid>
+        {locations.map(location => {
+          const status = isOpen(location);
+          const day = getDay(location.tz);
+          return (
+            <Styled.Location key={location.name}>
+              <Styled.LocationName>{location.name}</Styled.LocationName>
+              <Styled.LocationAddress
+                href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                  location.address
+                )},+Orlando+FL`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {location.address}
+              </Styled.LocationAddress>
+              <Styled.Status status={status}>{status}</Styled.Status>
+              {status !== 'Opening Soon' && (
+                <div>
+                  <Styled.Hours isToday={day > 1 && day < 6}>
+                    <Styled.Days>M–F</Styled.Days>
+                    <Styled.Range>
+                      {format(location.militaryHours[1][0])} –{' '}
+                      {format(location.militaryHours[1][1])}
+                    </Styled.Range>
+                  </Styled.Hours>
+                  <Styled.Hours isToday={day === 6}>
+                    <Styled.Days>Sat</Styled.Days>
+                    <Styled.Range>
+                      {format(location.militaryHours[6][0])} –{' '}
+                      {format(location.militaryHours[6][1])}
+                    </Styled.Range>
+                  </Styled.Hours>
+                  <Styled.Hours isToday={day === 0}>
+                    <Styled.Days>Sun</Styled.Days>
+                    <Styled.Range>
+                      {format(location.militaryHours[0][0])} –{' '}
+                      {format(location.militaryHours[0][1])}
+                    </Styled.Range>
+                  </Styled.Hours>
+                </div>
+              )}
+            </Styled.Location>
+          );
+        })}
+      </Styled.Grid>
+    </Styled.Container>
+  );
 };
-
-const StoreHours: React.FunctionComponent = () => (
-  <Styled.Container>
-    <Styled.Heading>Locations</Styled.Heading>
-    <Styled.Grid>
-      {locations.map(location => {
-        const status = isOpen(location);
-        return (
-          <Styled.Location key={location.name}>
-            <Styled.LocationName>{location.name}</Styled.LocationName>
-            <Styled.LocationAddress
-              href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                location.address
-              )},+Orlando+FL`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {location.address}
-            </Styled.LocationAddress>
-            <Styled.Status status={status}>{status}</Styled.Status>
-            {status !== 'Opening Soon' && (
-              <div>
-                <Styled.Hours isToday={day > 1 && day < 6}>
-                  <Styled.Days>M–F</Styled.Days>
-                  <Styled.Range>
-                    {format(location.militaryHours[1][0])} – {format(location.militaryHours[1][1])}
-                  </Styled.Range>
-                </Styled.Hours>
-                <Styled.Hours isToday={day === 6}>
-                  <Styled.Days>Sat</Styled.Days>
-                  <Styled.Range>
-                    {format(location.militaryHours[6][0])} – {format(location.militaryHours[6][1])}
-                  </Styled.Range>
-                </Styled.Hours>
-                <Styled.Hours isToday={day === 0}>
-                  <Styled.Days>Sun</Styled.Days>
-                  <Styled.Range>
-                    {format(location.militaryHours[0][0])} – {format(location.militaryHours[0][1])}
-                  </Styled.Range>
-                </Styled.Hours>
-              </div>
-            )}
-          </Styled.Location>
-        );
-      })}
-    </Styled.Grid>
-  </Styled.Container>
-);
 
 export default StoreHours;
