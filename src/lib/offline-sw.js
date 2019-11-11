12f81ae4fecb33c22a3749bbfe4b1dcdfe82fe00
Drew Powers <drew@pow.rs>
2019-11-11 10:28:23 -0700

    Remove SW

--- src/lib/offline-sw.js
+++
@@ -1,9 +0,0 @@
-/**
- * Offline service worker plugin
- * Keeps your site running while offline.
- * Runs in production only.
- */
-
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
-
-OfflinePluginRuntime.install();
